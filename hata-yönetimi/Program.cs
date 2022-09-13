d999b285f8e14e35e32677c588d905f0a96a6b53
Mehmetkozanoglu <108228905+Mehmetkozanoglu@users.noreply.github.com>
2022-09-13 21:57:03 +0300

    Hata-Yönetimi Konsol Projesi Oluşturuldu

---
+++ hata-yönetimi/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace hata_yönetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // try
+            // {
+            //   Console.WriteLine("Bir Sayı Giriniz:");
+            // int sayi = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Girmiş Olduğunuz Sayı :" + sayi);
+                 
+            // }
+            // catch(Exception ex)
+            // {
+            //    Console.WriteLine("Hata: "+ ex.Message.ToString());
+
+            // }
+            // finally
+            // {
+            // Console.WriteLine("İşlem Tamamlandı.");
+            // }
+
+            try
+            {
+              //  int a = int.Parse(null);
+              //  int a = int.Parse("test");
+              int a = int.Parse("-20000000000");
+              }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş Değer Girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri Tipi Uygun Değil.");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük ya da çok büyük bir değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally{
+                Console.WriteLine("İşlem Başarıyla Tamamlandı.");
+            }
+
+
+        }
+    }
+}
