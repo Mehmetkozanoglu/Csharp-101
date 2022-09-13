469f54cb867d5377a7800df2370ca71bca220c64
Mehmetkozanoglu <108228905+Mehmetkozanoglu@users.noreply.github.com>
2022-09-13 22:33:11 +0300

    Donguler-While-Foreach Konsol Projesi Oluşturuldu

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // While
+           // 1'den başlayarak console'dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console'a yazdıran program
+           // Console.Write("Lütfen bir sayı giriniz: ");
+           // int sayi = int.Parse(Console.ReadLine());
+          //  int sayac = 1;
+          //  int toplam = 0;
+          //  while (sayac <= sayi)
+          // {
+          //  toplam += sayac;
+          //  sayac ++;
+          // }
+          //   Console.WriteLine(toplam/sayi);
+
+        // a'dan z'ye kadar tüm harfleri console'a yazdıralım
+        // char character = 'a';
+        // while (character < 'z')
+        // {
+        //    Console.Write(character);
+        //    character ++;
+        // }
+
+         Console.Write("**** Foreach ****");
+         // Foreach döngüsü
+
+        string[] arabalar = {"BMW" , "Ford" , "Toyota" , "Seat"};
+
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+
+       }
+    }
+}
